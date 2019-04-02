6618ea29e2730937ea750d86ec52652e2ca69c25
tokio <tt.seed_oo.0829@i.softbank.jp>
2019-04-03 07:05:27 +0900

    JavaScriptでストップウォッチを作ろう

---
+++ MyStopwatch/js/main.js
@@ -0,0 +1,21 @@
+'use strict';
+
+{
+  const timer = document.getElementById('timer');
+  const start = document.getElementById('start');
+  const stop = document.getElementById('stop');
+  const reset = document.getElementById('reset');
+}
+let startTime;
+function countUp(){
+  console.log(Date.now() - startTime);
+  setTimeout(() =>{
+    countUp();
+  },10);
+}
+start.addEventListener('click',()=>{
+  startTime=Date.now();
+  countUp();{
+
+  }
+})
